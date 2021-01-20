fa9dc294e1fdf3f43f9fa560b720cfe70805cc3f
Bernhard Kirchen <schlimmchen@posteo.net>
2021-01-20 19:13:40 +0100

    use hammer-wrench icon from material design icon set

--- src/gui/dbsettings/DatabaseSettingsDialog.cpp
+++ src/gui/dbsettings/DatabaseSettingsDialog.cpp
@@ -117,7 +117,7 @@ DatabaseSettingsDialog::DatabaseSettingsDialog(QWidget* parent)
     m_ui->stackedWidget->addWidget(m_browserWidget);
 #endif
 
-    m_ui->categoryList->addCategory(tr("Maintenance"), icons()->icon("support"));
+    m_ui->categoryList->addCategory(tr("Maintenance"), icons()->icon("hammer-wrench"));
     m_ui->stackedWidget->addWidget(m_maintenanceWidget);
 
     pageChanged();
